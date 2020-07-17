1a30fbb1747e26edf5f5e9ce0d1d3f1ebf9529c4
takanamir <rtakanami0706@gmail.com>
2020-07-17 14:03:29 +0900

    Firebaseとの連携

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBTq63blsXwIfxoAzaEDBNyf0--pLcs4Ig",
+    authDomain: "angular-chat-8a797.firebaseapp.com",
+    databaseURL: "https://angular-chat-8a797.firebaseio.com",
+    projectId: "angular-chat-8a797",
+    storageBucket: "angular-chat-8a797.appspot.com",
+    messagingSenderId: "305327496488",
+    appId: "1:305327496488:web:c4e32e8e2a8979c814c41d",
+    measurementId: "G-RPBNCYV72G"
+  }
 };
 
 /*
