123c5c4a4ae5f521536d987c4453a50371f17971
takanamir <rtakanami0706@gmail.com>
2020-07-21 11:58:52 +0900

    ユーザー詳細ページを作成

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { NewUserComponent } from './new-user/new-user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 const routes: Routes = [
-  { path: 'new', component: NewUserComponent }
+  { path: 'new', component: NewUserComponent },
+  { path: ':id', component: UserDetailComponent }
 ];
 
 @NgModule({
@@ -12,8 +14,6 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [
-    // NewUserComponent
-  ]
+  declarations: []
 })
 export class UsersRoutingModule { }
